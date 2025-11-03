8077bd7f224c1749858d02609d3a081ae3a43f80
Valik <zuev.valentin.qa@gmail.com>
2025-11-03 18:56:23 +0200

    Detected mails

--- homeworks/tempCodeRunnerFile.js
+++ homeworks/tempCodeRunnerFile.js
@@ -1,1 +1,26 @@
-console.log((+null == false) < 1 ** 5);
+var arr = [
+  {
+    userName: "Test",
+    lastName: "Qwerty",
+    email: "test.mail@bigmir.net"
+  },
+  {
+    userName: "Valik",
+    lastName: "Zuiev",
+    email: "valik.zuiev@gmail.com"
+  },
+  {
+    userName: "Enemy",
+    lastName: "",
+    email: "enemy@mail.ru" //
+  },
+];
+
+// проверк email с gmail.com или bigmir.net
+const trustedEmailPattern = /^[a-zA-Z0-9._%+-]+@(gmail\.com|bigmir\.net)$/;
+
+// Фильтруем массив
+const trustedUsers = arr.filter(user => trustedEmailPattern.test(user.email));
+
+// Проверим результат
+console.log(trustedUsers);
